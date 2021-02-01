6438ccdeaa945fed1ad9ca5cd8a6b9e0b47e49c4
Nicolas Noé <nicolas@niconoe.eu>
2021-02-01 12:16:59 +0100

    Avoid URL warning

--- arabel/urls.py
+++ arabel/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    url(r'^$', include('website.urls')),
+    url(r'', include('website.urls')),
     path('admin/', admin.site.urls),
 ]
